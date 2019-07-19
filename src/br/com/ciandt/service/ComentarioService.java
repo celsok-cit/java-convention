30ad774bc3be0a85f42f0bf2067639c004c0ce22
Celso Katsuhiro Kushi <celsok@cit>
2019-07-19 11:24:19 -0300

    First commit

---
+++ src/br/com/ciandt/service/ComentarioService.java
@@ -0,0 +1,22 @@
+package br.com.ciandt.service;
+
+public class ComentarioService {
+
+	private final static String comentarioAdicionado = new String("Comentário adicionado!"); 
+	
+	public ComentarioService() {}
+	
+	public Boolean Adicionarlike (String comentario) 
+	{
+		
+		if(comentario != null){
+			return Boolean.TRUE;
+		}
+		
+		return Boolean.FALSE;
+		
+	}
+	public String _remover10(){
+		return comentarioAdicionado;		
+	}
+}
