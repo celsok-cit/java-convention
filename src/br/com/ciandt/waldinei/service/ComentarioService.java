2a9ec2ae18ded9429e8ffe43683a4802ff935642
Waldinei Pereira Gama <waldinei@lnxcit021860.cit>
2019-07-31 10:42:42 -0300

    Rename Package

--- src/br/com/ciandt/waldinei/service/ComentarioService.java
+++ src/br/com/ciandt/waldinei/service/ComentarioService.java
@@ -1,22 +1,32 @@
 package br.com.ciandt.waldinei.service;
 
+/**
+ * Classe de serviços para comentário
+ * 
+ * @author waldinei
+ */
 public class ComentarioService {
+	/**
+	 * Interface para retorno do comentário
+	 * 
+	 * @return Boolean - status comentário
+	 */
+	private final static String comentarioRemovido = new String("Comentário removido!");
 
-	private final static String comentarioRemovido = new String("Coment�rio removido!"); 
-	
-public ComentarioService() {}
-	
-	public Boolean Adicionarlike (String comentario) 
-	{
-		
-		if(comentario != null){
+	public ComentarioService() {
+	}
+
+	public Boolean Adicionarlike(String comentario) {
+
+		if (comentario != null) {
 			return Boolean.TRUE;
 		}
-		
+
 		return Boolean.FALSE;
-		
+
 	}
-	public String _remover10(){
-		return comentarioRemovido;		
+
+	public String _remover10() {
+		return comentarioRemovido;
 	}
 }
