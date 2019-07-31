ac1d218d8640eaddad31f40ab9245fe52649bd81
thiagodf <thiagodf@ciandt.com>
2019-07-31 10:13:39 -0300

    Renomeando packages

--- src/br/com/ciandt/thiagodf/models/Comentarios.java
+++ src/br/com/ciandt/thiagodf/models/Comentarios.java
@@ -1,10 +1,10 @@
-package br.com.ciandt.login.Models;
+package br.com.ciandt.thiagodf.models;
 
-public class Comentarios1 {
+public class Comentarios {
 
 	private String comentario;
 	
-	public Comentarios1() {}
+	public Comentarios() {}
 
 	public String getComentario() {
 		return comentario;
