7af29cba113ce5a86b3bdef11dd1706cf7f0ff94
emersondev <emersonmelody@hotmail.com>
2019-07-31 10:44:19 -0300

    Code Review

--- src/br/com/ciandt/enovais/service/ComentarioService.java
+++ src/br/com/ciandt/enovais/service/ComentarioService.java
@@ -1,22 +1,30 @@
 package br.com.ciandt.enovais.service;
 
+/**
+ * Classe de serviços para comentários Método Adicionarlike verifica que há uma
+ * String nula
+ * 
+ * @author enovais
+ */
 public class ComentarioService {
 
-	private final static String comentarioRemovido = new String("Comentário removido!"); 
-	
-public ComentarioService() {}
-	
-	public Boolean Adicionarlike (String comentario) 
-	{
-		
-		if(comentario != null){
+	private final static String comentarioRemovido = new String("Comentário removido!");
+
+	public ComentarioService() {
+	}
+
+	public Boolean Adicionarlike(String comentario) {
+		// se comentário for diferente de nulo, retorna true
+		if (comentario != null) {
 			return Boolean.TRUE;
 		}
-		
+
 		return Boolean.FALSE;
-		
+
 	}
-	public String _remover10(){
-		return comentarioRemovido;		
+
+	// remove o comentário
+	public String remover() {
+		return comentarioRemovido;
 	}
 }
