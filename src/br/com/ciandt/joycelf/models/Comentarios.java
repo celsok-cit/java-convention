cccad5987fa8ab51c835398d375afba09a802417
Joyce de Lana Lopes Ferreira <joycelf@lnxcit021680.cit>
2019-07-31 10:43:03 -0300

    Correção convenção Java

--- src/br/com/ciandt/joycelf/models/Comentarios.java
+++ src/br/com/ciandt/joycelf/models/Comentarios.java
@@ -1,10 +1,10 @@
-package br.com.ciandt.joycelf.Models;
+package br.com.ciandt.joycelf.models;
 
-public class Comentarios1 {
+public class Comentarios {
 
 	private String comentario;
-	
-	public Comentarios1() {}
+
+	public Comentarios() {}
 
 	public String getComentario() {
 		return comentario;
@@ -13,5 +13,5 @@ public class Comentarios1 {
 	public void setComentario(String comentario) {
 		this.comentario = comentario;
 	}
-	
+
 }
--- src/br/com/ciandt/joycelf/service/ComentarioService.java
+++ src/br/com/ciandt/joycelf/service/ComentarioService.java
@@ -1,22 +1,38 @@
 package br.com.ciandt.joycelf.service;
 
+/**
+ * Classe que controla os comentários
+ */
 public class ComentarioService {
 
-	private final static String comentarioRemovido = new String("Coment�rio removido!"); 
-	
-public ComentarioService() {}
-	
-	public Boolean Adicionarlike (String comentario) 
-	{
-		
-		if(comentario != null){
+	private final static String comentarioRemovido = new String("Coment�rio removido!");
+
+	/*
+	 * Construtor da classe
+	 */
+	public ComentarioService() {}
+
+	/**
+	 * Adiciona like
+	 * 
+	 * @param comentario
+	 * @return true ou false
+	 */
+	public Boolean adicionarLike(String comentario) {
+		// verifica se o comentario está nulo
+		if (comentario != null) {
 			return Boolean.TRUE;
 		}
-		
+
 		return Boolean.FALSE;
-		
 	}
-	public String _remover10(){
-		return comentarioRemovido;		
+
+	/**
+	 * Notifica que um comentário foi removido
+	 * 
+	 * @return string de comentário removido
+	 */
+	public String _removerComentario() {
+		return comentarioRemovido;
 	}
 }
