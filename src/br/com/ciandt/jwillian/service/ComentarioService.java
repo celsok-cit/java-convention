79888b46d8aa5f4f262088dd1c22e783c43e857d
Jonatas WF <Jonataswf@msn.com>
2019-07-31 10:39:06 -0300

    Inserção de comentarios da classe e metodos

--- src/br/com/ciandt/jwillian/service/ComentarioService.java
+++ src/br/com/ciandt/jwillian/service/ComentarioService.java
@@ -1,11 +1,25 @@
 package br.com.ciandt.jwillian.service;
 
+/**
+ * Classe de serviços para comentários
+ * @author jwillian
+ */
 public class ComentarioService {
 
+
 	private final static String comentarioRemovido = new String("Comentário removido!"); 
-	
+
+
+/**
+ * Metodo contrutor da classe
+ */
 public ComentarioService() {}
 	
+	/**
+	 * Metodo que verificar o comentario
+	 * @param comentario {@link String} comentário
+	 * @return true caso valor diferente de null ou false se for null
+	 */
 	public Boolean Adicionarlike (String comentario) 
 	{
 		
@@ -16,6 +30,11 @@ public ComentarioService() {}
 		return Boolean.FALSE;
 		
 	}
+	
+	/**
+	 * Metodo que remove comentario
+	 * @return comentário removido
+	 */
 	public String _remover10(){
 		return comentarioRemovido;		
 	}
